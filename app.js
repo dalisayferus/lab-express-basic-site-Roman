0e4d331178fa6dfd8898988d44dbe27a6aec5d05
Roman Kuptsov <romankuptsov@icloud.com>
2023-06-21 09:28:24 +0200

    fixed the dot problem on the home page with the links

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
- 
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home-page.html")
+);
 
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about-page.html'));
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about-page.html")
+);
 
-app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works-page.html'));
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works-page.html")
+);
 
-app.get('/photo-gallery', (request, response, next) => response.sendFile(__dirname + '/views/photo-gallery-page.html'));
+app.get("/photo-gallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/photo-gallery-page.html")
+);
 
 app.listen(3000);
